034b2a1e1cd33f93358d73f93619714a39ebfc9f
Andrew Hung <druphoria@utexas.edu>
2015-06-12 21:01:36 -0700

    imported spritesheet and implemented movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D rigidBody2D;
+
+    public float speed = 10;
+
+	void Start () {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        rigidBody2D.velocity = new Vector2(inputX * speed, inputY * speed);
+    }
+}
