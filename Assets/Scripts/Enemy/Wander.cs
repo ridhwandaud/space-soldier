122b9fcf3f42ddd56fdf58793499d648c2c3e2b3
Andrew Hung <druphoria@utexas.edu>
2015-06-18 21:56:51 -0700

    implemented a simple but effective wandering algorithm

---
+++ Assets/Scripts/Enemy/Wander.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wander : MonoBehaviour {
+
+    public Vector2 timeBetweenWanders = new Vector2(1.5f, 1.8f);
+    public float speed = 2f;
+
+    private Rigidbody2D rigidBody2D;
+    private float nextWanderTime;
+
+    private static Vector2 DEFAULT_ANGLE = new Vector2(1, 0);
+
+	void Start () {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        nextWanderTime = Time.time + Random.Range(0f, .3f);
+	}
+
+    Vector2 RotateVector(Vector2 vector, float radians)
+    {
+        return new Vector2(vector.x * Mathf.Cos(radians) - vector.y * Mathf.Sin(radians), 
+            vector.x * Mathf.Sin(radians) + vector.y * Mathf.Cos(radians));
+    }
+
+    void DoWander()
+    {
+        float angle = Random.Range(0f, 2 * Mathf.PI);
+        rigidBody2D.velocity = RotateVector(DEFAULT_ANGLE, angle).normalized * speed;
+    }
+	
+	void Update () {
+        if (Time.time > nextWanderTime)
+        {
+            nextWanderTime = Time.time + Random.Range(timeBetweenWanders.x, timeBetweenWanders.y);
+            DoWander();
+        }
+	}
+}
