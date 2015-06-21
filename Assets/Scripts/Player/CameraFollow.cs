63c50dd499ef705d18ac739e6aa28bf12a03a652
Andrew Hung <druphoria@utexas.edu>
2015-06-20 23:21:09 -0700

    added smooth camera follow

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -5,14 +5,17 @@ public class CameraFollow : MonoBehaviour {
     private GameObject player;
 
     private static float DEFAULT_Z = -10;
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
 
     void Awake()
     {
         player = GameObject.Find("Soldier");
     }
 
-	void Update () {
-        Vector3 playerPosition = player.transform.position;
-        transform.position = new Vector3(playerPosition.x, playerPosition.y, DEFAULT_Z);
-	}
+	void FixedUpdate () {
+        Vector3 targetPosition = player.transform.position;
+        targetPosition.z = DEFAULT_Z;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, dampTime);
+    }
 }
