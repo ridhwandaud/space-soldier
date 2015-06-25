a953fdd56105a858049a335274f0418a3d6cec98
Andrew Hung <druphoria@utexas.edu>
2015-06-24 23:17:23 -0700

    bare bones health script

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int healthPoints = 3;
+
+	void OnTriggerEnter2D (Collider2D other) {
+        if (other.tag.Equals("Fireball"))
+        {
+            healthPoints--;
+            other.GetComponent<ProjectileDestroy>().Destroy();
+        }
+
+        if (healthPoints == 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
