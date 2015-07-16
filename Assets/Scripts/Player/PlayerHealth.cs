b52d364ca894b4ed36eb7682cb1f7c2154644619
Andrew Hung <druphoria@utexas.edu>
2015-07-15 23:07:58 -0700

    added some textures and created a bare bones health bar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
-    public int healthPoints = 3;
+    public int healthPoints = 30;
+    public Slider healthSlider;
 
 	void OnTriggerEnter2D (Collider2D other) {
         if (other.tag.Equals("Fireball"))
@@ -11,6 +13,9 @@ public class PlayerHealth : MonoBehaviour {
             healthPoints--;
             other.GetComponent<ProjectileDestroy>().Destroy();
         }
+
+        // Only do this if the player takes damage?
+        healthSlider.value = healthPoints;
 
         if (healthPoints == 0)
         {
