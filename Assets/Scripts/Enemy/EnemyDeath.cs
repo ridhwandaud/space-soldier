982877fa9d91dceb57a44313f82b112545b25ec5
Andrew Hung <druphoria@utexas.edu>
2015-07-30 00:00:37 -0700

    leveling/experience framework

--- Assets/Scripts/Enemy/EnemyDeath.cs
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -4,16 +4,23 @@ using System.Collections;
 public class EnemyDeath : MonoBehaviour {
     public int experiencePoints;
 
-    // To increment experience?
-    private GameObject player;
+    private PlayerExperience playerExperience;
+    private bool destroyed = false;
 
     void Awake()
     {
-        player = GameObject.Find("Soldier");
+        playerExperience = GameObject.Find("Soldier").GetComponent<PlayerExperience>();
     }
 
     public void KillEnemy()
     {
-        Destroy(gameObject);
+        // Destroy doesn't actually take effect until after the update loop, so I added this condition to prevent explosive damage (and other forms
+        // of secondary damage) from doubling the experience points earned.
+        if (!destroyed)
+        {
+            playerExperience.IncrementExperience(experiencePoints);
+            Destroy(gameObject);
+            destroyed = true;
+        }
     }
 }
---
+++ Assets/Scripts/Player/PlayerExperience.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerExperience : MonoBehaviour {
+
+    private int experiencePoints = 0;
+    private int experienceToLevelUp = 30;
+
+    public int Level { get; set; }
+    public int AvailableSkillPoints { get; set; }
+
+    public void IncrementExperience(int experiencePointsGained)
+    {
+        experiencePoints += experiencePointsGained;
+        print("Gained " + experiencePointsGained + " experience. Now at " + experiencePoints);
+        // TODO: Slider stuff.
+
+        if (experiencePoints >= experienceToLevelUp)
+        {
+            experiencePoints = experiencePoints - experienceToLevelUp;
+            LevelUp();
+            experienceToLevelUp = calculateExperienceToLevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        Level++;
+        AvailableSkillPoints++;
+        // TODO: Trigger some other behaviors, such as indicating on the GUI that a new level was achieved.
+    }
+
+    private int calculateExperienceToLevelUp()
+    {
+        //int targetLevel = Level + 1;
+        //return targetLevel * 100;
+        return 30;
+    }
+}
