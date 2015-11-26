5045bf93890914f77d49e97a9eef12aa2c11af72
Andrew Hung <druphoria@utexas.edu>
2015-11-26 10:25:50 -0500

    adding tabs to switch between skill tree panels

---
+++ Assets/Scripts/UI/SkillTreeTab.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SkillTreeTab : MonoBehaviour, IPointerClickHandler {
+    // These will eventually be replaced by different frames.
+    private static Color ActiveColor = Color.green;
+    private static Color InactiveColor = Color.gray;
+
+    [SerializeField]
+    private GameObject skillTreeBody;
+
+    // Sadly, these cannot be retrieved programmatically because they start off disabled.
+    [SerializeField]
+    private GameObject[] otherSkillTreeTabs;
+    [SerializeField]
+    private GameObject[] otherSkillTreeBodies;
+    private Image image;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        image.color = skillTreeBody.activeSelf ? ActiveColor : InactiveColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        for (int i = 0; i < otherSkillTreeTabs.Length; i++)
+        {
+            otherSkillTreeTabs[i].GetComponent<Image>().color = InactiveColor;
+            otherSkillTreeBodies[i].SetActive(false);
+        }
+        skillTreeBody.SetActive(true);
+        image.color = ActiveColor;
+    }
+}
