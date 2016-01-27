f15e640a2fc047d4679697be13e0abe880c649fc
Andrew W Hung <druphoria@utexas.edu>
2016-01-26 19:23:41 -0800

    added crate

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,7 @@ using SpriteTile;
 
 public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
 
     public float speed = 8;
     public float collisionDistance;
@@ -10,9 +11,10 @@ public class PlayerMovement : MonoBehaviour {
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
 	}
 
-    void FixedUpdate()
+    void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
@@ -27,28 +29,36 @@ public class PlayerMovement : MonoBehaviour {
 
         if ((Tile.GetCollider(new Vector2(rb.position.x, rb.position.y + collisionDistance), wallSpriteTileLayer)
             || Tile.GetCollider(new Vector2(rb.position.x - halfCollisionDistance, rb.position.y + collisionDistance), wallSpriteTileLayer)
-            || Tile.GetCollider(new Vector2(rb.position.x + halfCollisionDistance, rb.position.y + collisionDistance), wallSpriteTileLayer))
+            || Tile.GetCollider(new Vector2(rb.position.x + halfCollisionDistance, rb.position.y + collisionDistance), wallSpriteTileLayer)
+            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.up, halfCollisionDistance,
+                LayerMasks.ObstacleLayerMask).transform != null)
             && inputY > 0)
         {
             newVelocity.y = 0;
         }
         if ((Tile.GetCollider(new Vector2(rb.position.x, rb.position.y - collisionDistance), wallSpriteTileLayer)
             || Tile.GetCollider(new Vector2(rb.position.x - halfCollisionDistance, rb.position.y - collisionDistance), wallSpriteTileLayer)
-            || Tile.GetCollider(new Vector2(rb.position.x + halfCollisionDistance, rb.position.y - collisionDistance), wallSpriteTileLayer))
+            || Tile.GetCollider(new Vector2(rb.position.x + halfCollisionDistance, rb.position.y - collisionDistance), wallSpriteTileLayer)
+            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.down, halfCollisionDistance,
+                LayerMasks.ObstacleLayerMask).transform != null)
             && inputY < 0)
         {
             newVelocity.y = 0;
         }
         if ((Tile.GetCollider(new Vector2(rb.position.x + collisionDistance, rb.position.y), wallSpriteTileLayer)
             || Tile.GetCollider(new Vector2(rb.position.x + collisionDistance, rb.position.y - halfCollisionDistance), wallSpriteTileLayer)
-            || Tile.GetCollider(new Vector2(rb.position.x + collisionDistance, rb.position.y + halfCollisionDistance), wallSpriteTileLayer))
+            || Tile.GetCollider(new Vector2(rb.position.x + collisionDistance, rb.position.y + halfCollisionDistance), wallSpriteTileLayer)
+            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.right, halfCollisionDistance,
+                LayerMasks.ObstacleLayerMask).transform != null)
             && inputX > 0)
         {
             newVelocity.x = 0;
         }
         if ((Tile.GetCollider(new Vector2(rb.position.x - collisionDistance, rb.position.y), wallSpriteTileLayer)
             || Tile.GetCollider(new Vector2(rb.position.x - collisionDistance, rb.position.y - halfCollisionDistance), wallSpriteTileLayer)
-            || Tile.GetCollider(new Vector2(rb.position.x - collisionDistance, rb.position.y + halfCollisionDistance), wallSpriteTileLayer))
+            || Tile.GetCollider(new Vector2(rb.position.x - collisionDistance, rb.position.y + halfCollisionDistance), wallSpriteTileLayer)
+            || Physics2D.BoxCast(rb.position, boxCollider.size, 0f, Vector2.left, halfCollisionDistance,
+                LayerMasks.ObstacleLayerMask).transform != null)
             && inputX < 0)
         {
             newVelocity.x = 0;
--- Assets/Util/LayerMasks.cs
+++ Assets/Util/LayerMasks.cs
@@ -2,4 +2,5 @@
 	public static int WallLayerMask = 1 << 8;
     public static int EnemyLayerMask = 1 << 9;
     public static int MissileLayerMask = 1 << 10;
+    public static int ObstacleLayerMask = 1 << 12;
 }
